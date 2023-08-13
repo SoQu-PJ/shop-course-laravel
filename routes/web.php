b86461ab0cefaaae2c5da969fa7816114c19a760
SoQu-PJ <jakobowskipawel02@gmail.com>
2023-08-13 11:39:09 +0200

    Create show users list
    create userController add to routing where use index function to show index.blade.php, use model to show users in table(bootstrap), proctected users/list in routing by middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/list', [UserController::class, 'index'])->middleware('auth');
 Route::get('/hello', [HelloController::class, 'show']);
 
 Auth::routes();
